7279289788033303673549dcdd5923367cc575ef
SatishNagare <satish.nagare@gmail.com>
2020-09-30 21:53:24 -0400

    Adding asynchronicity

--- src/SimpleWebApi/Controllers/WeatherForecastController.cs
+++ src/SimpleWebApi/Controllers/WeatherForecastController.cs
@@ -24,16 +24,17 @@ namespace SimpleWebApi.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<IEnumerable<WeatherForecast>> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var output = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            });
+            var result = await Task.FromResult(output.ToArray());
+            return result;
         }
     }
 }
